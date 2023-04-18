8b8e7e63fe5603a698c2e71f7bfb0ba2238190b8
Heba-Mohammad <116072053+Heba-Mohammad@users.noreply.github.com>
2023-04-18 12:52:57 +0300

    add chrome headless in our project

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -82,18 +83,29 @@ public class TestBase extends AbstractTestNGCucumberTests
 			driver = new EdgeDriver();
 		}
 		//headless browser testing
-		
+
 		else if(browserName.equalsIgnoreCase("headless"))
 		{
 			DesiredCapabilities caps= new DesiredCapabilities();
 			caps.setJavascriptEnabled(true);
 
 			caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY
-				             	,System.getProperty("user.dir")+"/drivers/phantomjs.exe");
+					,System.getProperty("user.dir")+"/drivers/phantomjs.exe");
 			String[] phantomJsArgs= {"--web-security=no","--ignore--ssl-errors=yes"};
 			caps.setCapability(PhantomJSDriverService.PHANTOMJS_GHOSTDRIVER_CLI_ARGS, phantomJsArgs);
 			driver=new PhantomJSDriver(caps);
+
+		}
+		else if(browserName.equalsIgnoreCase("chrome-headless"))
+		{
 			
+			ChromeOptions options= new ChromeOptions();
+			options.addArguments("--start-maximized");
+			options.addArguments("--remote-allow-origins=*");
+			
+			options.addArguments("--headless");
+			options.addArguments("--windows-size=1920,1080");
+			driver= new ChromeDriver(options);
 		}
 
 		driver.manage().window().maximize();
